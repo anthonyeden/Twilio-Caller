7ade58cd24b4f3bba5922acc9b211a40e370ee80
Anthony Eden <anthony@mediarealm.com.au>
2017-04-14 18:49:59 +1000

    Include version file & update build notes.

---
+++ version.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(1, 1, 1, 1),
+    prodvers=(1, 1, 1, 1),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'Anthony Eden / Media Realm'),
+        StringStruct(u'FileDescription', u'Twilio Caller'),
+        StringStruct(u'FileVersion', u'1.1.1)'),
+        StringStruct(u'InternalName', u'TwilioCaller'),
+        StringStruct(u'LegalCopyright', u'\xa9 Anthony Eden. All rights reserved.'),
+        StringStruct(u'OriginalFilename', u'TwilioCaller.Exe'),
+        StringStruct(u'ProductName', u'Twilio Caller'),
+        StringStruct(u'ProductVersion', u'1.1.1.1')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
